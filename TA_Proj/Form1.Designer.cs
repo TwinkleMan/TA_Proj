0ed384ca4246cac4233678dbe4c8b57de6feab84
Artem <artemesin2016@gmail.com>
2020-12-28 12:55:27 +0700

    Initial commit

---
+++ TA_Proj/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace TA_Proj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.player1label = new System.Windows.Forms.Label();
+            this.player2label = new System.Windows.Forms.Label();
+            this.player1throw = new System.Windows.Forms.Button();
+            this.player2throw = new System.Windows.Forms.Button();
+            this.Flushbtn = new System.Windows.Forms.Button();
+            this.nextbtn = new System.Windows.Forms.Button();
+            this.score_text = new System.Windows.Forms.Label();
+            this.scorelbl = new System.Windows.Forms.Label();
+            this.res1_text = new System.Windows.Forms.Label();
+            this.player1res_lbl = new System.Windows.Forms.Label();
+            this.player2res_lbl = new System.Windows.Forms.Label();
+            this.res2_text = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // player1label
+            // 
+            this.player1label.Location = new System.Drawing.Point(77, 175);
+            this.player1label.Name = "player1label";
+            this.player1label.Size = new System.Drawing.Size(59, 28);
+            this.player1label.TabIndex = 0;
+            this.player1label.Text = "Игрок 1";
+            // 
+            // player2label
+            // 
+            this.player2label.Location = new System.Drawing.Point(672, 175);
+            this.player2label.Name = "player2label";
+            this.player2label.Size = new System.Drawing.Size(59, 28);
+            this.player2label.TabIndex = 1;
+            this.player2label.Text = "Игрок 2";
+            // 
+            // player1throw
+            // 
+            this.player1throw.Location = new System.Drawing.Point(57, 206);
+            this.player1throw.Name = "player1throw";
+            this.player1throw.Size = new System.Drawing.Size(100, 42);
+            this.player1throw.TabIndex = 2;
+            this.player1throw.Text = "Бросок";
+            this.player1throw.UseVisualStyleBackColor = true;
+            this.player1throw.Click += new System.EventHandler(this.player1throw_Click);
+            // 
+            // player2throw
+            // 
+            this.player2throw.Location = new System.Drawing.Point(651, 206);
+            this.player2throw.Name = "player2throw";
+            this.player2throw.Size = new System.Drawing.Size(100, 42);
+            this.player2throw.TabIndex = 3;
+            this.player2throw.Text = "Бросок";
+            this.player2throw.UseVisualStyleBackColor = true;
+            this.player2throw.Click += new System.EventHandler(this.player2throw_Click_1);
+            // 
+            // Flushbtn
+            // 
+            this.Flushbtn.Location = new System.Drawing.Point(232, 356);
+            this.Flushbtn.Name = "Flushbtn";
+            this.Flushbtn.Size = new System.Drawing.Size(128, 69);
+            this.Flushbtn.TabIndex = 4;
+            this.Flushbtn.Text = "Сброс";
+            this.Flushbtn.UseVisualStyleBackColor = true;
+            this.Flushbtn.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Flushbtn_MouseClick);
+            // 
+            // nextbtn
+            // 
+            this.nextbtn.Location = new System.Drawing.Point(429, 356);
+            this.nextbtn.Name = "nextbtn";
+            this.nextbtn.Size = new System.Drawing.Size(128, 69);
+            this.nextbtn.TabIndex = 5;
+            this.nextbtn.Text = "Следующий раунд";
+            this.nextbtn.UseVisualStyleBackColor = true;
+            this.nextbtn.Click += new System.EventHandler(this.nextbtn_Click);
+            // 
+            // score_text
+            // 
+            this.score_text.Location = new System.Drawing.Point(356, 34);
+            this.score_text.Name = "score_text";
+            this.score_text.Size = new System.Drawing.Size(49, 29);
+            this.score_text.TabIndex = 6;
+            this.score_text.Text = "Счет:";
+            // 
+            // scorelbl
+            // 
+            this.scorelbl.Location = new System.Drawing.Point(399, 34);
+            this.scorelbl.Name = "scorelbl";
+            this.scorelbl.Size = new System.Drawing.Size(46, 29);
+            this.scorelbl.TabIndex = 7;
+            this.scorelbl.Text = "0 / 0";
+            // 
+            // res1_text
+            // 
+            this.res1_text.Location = new System.Drawing.Point(74, 267);
+            this.res1_text.Name = "res1_text";
+            this.res1_text.Size = new System.Drawing.Size(100, 24);
+            this.res1_text.TabIndex = 8;
+            this.res1_text.Text = "Выпало:";
+            // 
+            // player1res_lbl
+            // 
+            this.player1res_lbl.Location = new System.Drawing.Point(74, 291);
+            this.player1res_lbl.Name = "player1res_lbl";
+            this.player1res_lbl.Size = new System.Drawing.Size(61, 22);
+            this.player1res_lbl.TabIndex = 9;
+            // 
+            // player2res_lbl
+            // 
+            this.player2res_lbl.Location = new System.Drawing.Point(670, 291);
+            this.player2res_lbl.Name = "player2res_lbl";
+            this.player2res_lbl.Size = new System.Drawing.Size(61, 22);
+            this.player2res_lbl.TabIndex = 11;
+            // 
+            // res2_text
+            // 
+            this.res2_text.Location = new System.Drawing.Point(670, 267);
+            this.res2_text.Name = "res2_text";
+            this.res2_text.Size = new System.Drawing.Size(100, 24);
+            this.res2_text.TabIndex = 10;
+            this.res2_text.Text = "Выпало:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.player2res_lbl);
+            this.Controls.Add(this.res2_text);
+            this.Controls.Add(this.player1res_lbl);
+            this.Controls.Add(this.res1_text);
+            this.Controls.Add(this.scorelbl);
+            this.Controls.Add(this.score_text);
+            this.Controls.Add(this.nextbtn);
+            this.Controls.Add(this.Flushbtn);
+            this.Controls.Add(this.player2throw);
+            this.Controls.Add(this.player1throw);
+            this.Controls.Add(this.player2label);
+            this.Controls.Add(this.player1label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Label player2res_lbl;
+        private System.Windows.Forms.Label res2_text;
+
+        private System.Windows.Forms.Label player1res_lbl;
+
+        private System.Windows.Forms.Label res1_text;
+
+        private System.Windows.Forms.Label scorelbl;
+
+        private System.Windows.Forms.Label score_text;
+
+        private System.Windows.Forms.Button Flushbtn;
+        private System.Windows.Forms.Button nextbtn;
+
+        private System.Windows.Forms.Button player1throw;
+        private System.Windows.Forms.Button player2throw;
+
+        private System.Windows.Forms.Label player2label;
+
+        private System.Windows.Forms.Label player1label;
+
+        #endregion
+    }
+}
---
+++ TA_Proj/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+using System.Windows.Forms;
+
+namespace TA_Proj
+{
+    public partial class Form1 : Form
+    {
+        private bool player1 { get; set; }
+        private bool player2 { get; set; }
+
+        private int[] roundResults = new int[2];
+        private int player1Score = 0;
+        private int player2Score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            nextbtn.Enabled = false;
+        }
+
+        private void printResult(int playerNumber, int result)
+        {
+            
+            
+            if (playerNumber == 1)
+            {
+                if (result == 0)
+                {
+                    player1res_lbl.Text = "Решка";
+                }
+                else if (result == 1)
+                {
+                    player1res_lbl.Text = "Орел";
+                }
+            }
+            else if (playerNumber == 2)
+            {
+                if (result == 0)
+                {
+                    player2res_lbl.Text = "Решка";
+                }
+                else if (result == 1)
+                {
+                    player2res_lbl.Text = "Орел";
+                }
+            } 
+        }
+
+        private void refreshScore()
+        {
+            bool result = roundResults[0] != roundResults[1];
+
+            if (result)
+            {
+                player2Score += 1;
+            }
+            else
+            {
+                player1Score += 1;
+            }
+
+            scorelbl.Text = player1Score + " / " + player2Score;
+
+            if (player1Score == 2)
+            {
+                MessageBox.Show("Игрок 1 выиграл!", "Конец игры", MessageBoxButtons.OK);
+                restartGame();
+            } else if (player2Score == 2)
+            {
+                MessageBox.Show("Игрок 2 выиграл!", "Конец игры", MessageBoxButtons.OK);
+                restartGame();
+            }
+        }
+
+        private void restartGame()
+        {
+            player1 = false;
+            player2 = false;
+            player1Score = 0;
+            player2Score = 0;
+            player1throw.Enabled = true;
+            player2throw.Enabled = true;
+            nextbtn.Enabled = false;
+            scorelbl.Text = "0 / 0";
+            player1res_lbl.Text = "";
+            player2res_lbl.Text = "";
+        }
+
+        private void player1throw_Click(object sender, EventArgs e)
+        {
+            Random generator = new Random();
+
+            int coin = generator.Next(0, 2);
+            player1 = true;
+            roundResults[0] = coin;
+            
+            printResult(1,coin);
+            player1throw.Enabled = false;
+            if (player1 && player2)
+            {
+                nextbtn.Enabled = true;
+                refreshScore();
+            }
+        }
+
+        private void player2throw_Click_1(object sender, EventArgs e)
+        {
+            Random generator = new Random();
+
+            int coin = generator.Next(0, 2);
+            player2 = true;
+            roundResults[1] = coin;
+            
+            printResult(2,coin);
+            player2throw.Enabled = false;
+            if (player1 && player2)
+            {
+                nextbtn.Enabled = true;
+                refreshScore();
+            }
+        }
+
+        private void Flushbtn_MouseClick(object sender, MouseEventArgs e)
+        {
+            restartGame();
+        }
+
+        private void nextbtn_Click(object sender, EventArgs e)
+        {
+            player1 = false;
+            player2 = false;
+            player1throw.Enabled = true;
+            player2throw.Enabled = true;
+            nextbtn.Enabled = false;
+            player1res_lbl.Text = "";
+            player2res_lbl.Text = "";
+        }
+    }
+}
